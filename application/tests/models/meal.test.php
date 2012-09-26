93a349bd0f353da49612761aeaac156a5e216215
Ting Wang <tting.wang@gmail.com>
2012-09-26 21:42:17 +0800

    Initial the project of sjw for managing the meal ordering.

---
+++ application/tests/models/meal.test.php
@@ -0,0 +1,21 @@
+<?php
+
+class MealTest extends PHPUnit_Framework_TestCase
+{
+    public function testSave()
+    {
+        $meal = new Meal();
+        $meal->id = 1;
+        $meal->name = 'beef';
+        $meal->description = 'beef with potat';
+        $meal->price = 20.00;
+        $meal->save();
+
+        $mealFound = Meal::find(1);
+        $this->assertEquals($meal->name, $mealFound->name);
+        $this->assertEquals($meal->description, $mealFound->description);
+        $this->assertEquals($meal->price, $mealFound->price);
+        $mealFound->delete();
+    }
+}
+
